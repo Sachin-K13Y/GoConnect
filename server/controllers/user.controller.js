30a4cae61e8dcc5caef9c8e8baa02273bc4285a2
Sachin-K13Y <sachink13y@gmail.com>
2025-07-11 08:54:27 +0530

    Login Route Controller done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,7 +5,7 @@ import User from '../models/user.model.js';
 
 export const registerUser = async (req, res, next) => {
     try {
-        const { firstname, lastname, email, password } = req.body;
+        const { fullname, email, password } = req.body;
 
         
         const existingUser = await User.findOne({ email });
@@ -16,15 +16,13 @@ export const registerUser = async (req, res, next) => {
         
         const hashedPassword = bcrypt.hashSync(password, 10);
 
-    
         const user = new User({
-            firstname,
-            lastname,
+            fullname,
             email,
             password: hashedPassword
         });
 
-        // await user.save();
+        await user.save();
 
         const token = jwt.sign(
             { id: user._id },
@@ -47,3 +45,26 @@ export const registerUser = async (req, res, next) => {
         next(error);
     }
 };
+
+export const loginUser = async(req,res,next)=>{
+
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(401).json({message:'Invalid Email or Password'});
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        return res.status(401).json({message:'Invalid email or password'});
+    }
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+    res.status(200).json({"message":"User Login Successfully",token,user});
+    
+}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { registerUser } from '../controllers/user.controller.js';
+import { loginUser, registerUser } from '../controllers/user.controller.js';
 
 const userRoute = express.Router();
 
 userRoute.post('/register',registerUser)
+userRoute.post('/login',loginUser)
 
 export default userRoute;
